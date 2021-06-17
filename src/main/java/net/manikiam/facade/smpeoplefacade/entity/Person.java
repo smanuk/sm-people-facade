ae23ca3ec1a6054dd36b320d02534e35556f5250
Steven Manikiam <sman@manikiam.net>
2021-06-17 18:34:35 +0100

    Include the person entity.

---
+++ src/main/java/net/manikiam/facade/smpeoplefacade/entity/Person.java
@@ -0,0 +1,32 @@
+package net.manikiam.facade.smpeoplefacade.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Person {
+
+    private Long id;
+
+    @NotBlank(message = "First name must not be blank")
+    private String firstName;
+
+    @NotBlank(message = "Last name must not be blank")
+    private String lastName;
+
+    @NotNull(message = "Date of birth must not be null")
+    private LocalDate dateOfBirth;
+
+    @NotBlank(message = "Country code must not be blank")
+    private String countryCode;
+}
