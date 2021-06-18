fcd0f0c3f921289f2677bc81b08c62ce12f15364
Steven Manikiam <sman@manikiam.net>
2021-06-18 09:33:58 +0100

    New feign call to rest countries API for returning country information given a (3 or 2 letter) country code.

---
+++ src/main/java/net/manikiam/facade/smpeoplefacade/boundary/RestCountriesClient.java
@@ -0,0 +1,13 @@
+package net.manikiam.facade.smpeoplefacade.boundary;
+
+import net.manikiam.facade.smpeoplefacade.entity.dto.CountryInfo;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "countires", url = "${rest.person-service.url}")
+public interface RestCountriesClient {
+
+    @GetMapping("/v2/alpha/{code}")
+    CountryInfo getCountry(@PathVariable("code") String code);
+}
