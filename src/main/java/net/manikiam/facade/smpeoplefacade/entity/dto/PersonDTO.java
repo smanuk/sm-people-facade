2856ace04c7026fcb35d7a601821cf44d8a1f1b2
Steven Manikiam <sman@manikiam.net>
2021-06-18 10:07:51 +0100

    added Lombok all arg constructor to DTO

--- src/main/java/net/manikiam/facade/smpeoplefacade/entity/dto/PersonDTO.java
+++ src/main/java/net/manikiam/facade/smpeoplefacade/entity/dto/PersonDTO.java
@@ -1,9 +1,11 @@
 package net.manikiam.facade.smpeoplefacade.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.manikiam.facade.smpeoplefacade.entity.Person;
 
 @Data
+@AllArgsConstructor
 public class PersonDTO {
 
     private Person person;
