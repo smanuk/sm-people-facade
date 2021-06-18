c54e05bf62580cfebc7d57b77d18c6b0f4fc11ad
Steven Manikiam <sman@manikiam.net>
2021-06-18 09:31:25 +0100

    DTO for capturing information from rest-countries endpoints.

---
+++ src/main/java/net/manikiam/facade/smpeoplefacade/entity/dto/CountryInfo.java
@@ -0,0 +1,12 @@
+package net.manikiam.facade.smpeoplefacade.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class CountryInfo {
+
+    private String name;
+    private String alpha2Code;
+    private String capital;
+    private String region;
+}
