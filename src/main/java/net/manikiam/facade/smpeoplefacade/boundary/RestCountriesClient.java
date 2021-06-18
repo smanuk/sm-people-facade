560f8d8591eb07858b4356f58bae99cdf945f652
Steven Manikiam <sman@manikiam.net>
2021-06-18 13:10:46 +0100

    add caching to our rest countries queries

--- src/main/java/net/manikiam/facade/smpeoplefacade/boundary/RestCountriesClient.java
+++ src/main/java/net/manikiam/facade/smpeoplefacade/boundary/RestCountriesClient.java
@@ -1,6 +1,7 @@
 package net.manikiam.facade.smpeoplefacade.boundary;
 
 import net.manikiam.facade.smpeoplefacade.entity.dto.CountryInfo;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "countires", url = "${rest.rest-countries.url}")
 public interface RestCountriesClient {
 
+    @Cacheable("countryCode")
     @GetMapping("/v2/alpha/{code}")
     CountryInfo getCountry(@PathVariable("code") String code);
 }
