6372eb71caca65c716c93edea3e532d8828b2ec2
Steven Manikiam <sman@manikiam.net>
2021-06-18 13:33:06 +0100

    Script to load my test users into the system by using the facade post api.

--- loadUsers.sh
+++ loadUsers.sh
@@ -2,6 +2,15 @@
 
 while read -r person; do
 
-  echo $person | jq '.'
+  countryName=$(echo $person | jq -r '.country')
 
-done < <(jq -c '.results[] | {firstName: .name.first, lastName: .name.last, countryCode: }' $1)
+  # look up country code from rescountires
+  code=$(curl -s https://restcountries.eu/rest/v2/name/${countryName} | jq -r '.[0].alpha2Code')
+  echo $code
+  # Insert country code into person payload
+  person=$(echo "$person" | jq -c --arg code $code '.countryCode=$code')
+
+  # Send payload into facade post API
+  curl -s -XPOST -H "Content-Type: application/json" -d ${person} http://localhost:30000/v1/people
+
+done < <(jq -c '.results[] | {firstName: .name.first, lastName: .name.last, country: .location.country, dateOfBirth: .dob.date, countryCode: null} ' $1)
