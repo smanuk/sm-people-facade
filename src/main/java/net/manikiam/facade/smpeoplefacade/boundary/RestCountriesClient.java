c226499f8ef004776f8b5867e5aab7ce30a32acc
Steven Manikiam <sman@manikiam.net>
2021-06-18 10:07:19 +0100

    fixed rest countries URL issue

--- src/main/java/net/manikiam/facade/smpeoplefacade/boundary/RestCountriesClient.java
+++ src/main/java/net/manikiam/facade/smpeoplefacade/boundary/RestCountriesClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "countires", url = "${rest.person-service.url}")
+@FeignClient(name = "countires", url = "${rest.rest-countries.url}")
 public interface RestCountriesClient {
 
     @GetMapping("/v2/alpha/{code}")
